0f49dc7e7d5d65fa2f390d4754ae64bf774e164c
TheJaimex <41478167+TheJaimex@users.noreply.github.com>
2021-07-30 03:00:52 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var projectImages = document.querySelectorAll("#projectImage");
 projectImages.forEach( function(projectImage) {
         let ctx = projectImage.getContext("2d");
         let image = new Image();
-        image.src = "./TestWeb.webp";
+        image.src = "TestWeb.png";
 
         image.addEventListener("load", function() {
             ctx.drawImage(image, 0, 0);
