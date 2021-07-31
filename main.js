c171d5b5c2a5c427afb983ba0cee427f5de89064
TheJaimex <41478167+TheJaimex@users.noreply.github.com>
2021-08-01 00:45:00 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ projectImages.forEach( function(projectImage) {
     })
 
 
+// BOTON COPIAR EMAIL
+
+var copyEmailButton = document.querySelector("#copyEmailButton");
+copyEmailButton.addEventListener("click", copyEmailFromButton);
+
+function copyEmailFromButton() {
+    const email = "testemail@gmail.com";
+
+    navigator.clipboard.writeText(email).catch((error) => { alert(`Error copying: ${error}. The email is ${email}`) });
+
+    copyEmailButton.innerHTML = "Copied!";
+    copyEmailButton.removeEventListener("click", copyEmailFromButton);
+
+    setTimeout( function() {
+        copyEmailButton.innerHTML = "Copy Email";
+        copyEmailButton.addEventListener("click", copyEmailFromButton);
+    }, 2500);
+}
